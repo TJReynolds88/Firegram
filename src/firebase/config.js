db9432283ac028751abf238aa4000d214aa51d32
TJ Reynolds <tj.reynolds1988@gmail.com>
2020-08-03 23:05:19 -0500

    Add files via upload

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCDh3fzvV6m_3V4QgPfCcRzj3Swzu6Tegs",
+    authDomain: "firegram-61ba9.firebaseapp.com",
+    databaseURL: "https://firegram-61ba9.firebaseio.com",
+    projectId: "firegram-61ba9",
+    storageBucket: "firegram-61ba9.appspot.com",
+    messagingSenderId: "409092703620",
+    appId: "1:409092703620:web:0cc5f8674200edc534c9d1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projectStorage = firebase.storage();
+  const projectFireStore = firebase.firestore();
+
+  export { projectFireStore, projectFireStore};
+  
